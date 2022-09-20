09b4b1f6ee000254adb1c12eeafdc24047fe9ad7
Kevin cuadros <kevindavidcuadros@gmail.com>
2022-09-19 21:00:07 -0500

    sass colors

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,9 @@
   </div>
   <h1>Hola Mundo</h1>
   <button class="btn btn-primary" >Primary</button>
+  <button class="btn btn-secondary" >Secondary</button>
+  <button class="btn btn-success" >Success</button>
+
 </template>
 
 <script>
