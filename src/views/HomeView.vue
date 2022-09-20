2ca051ed001381f0d0ad01aa4b3247e5373dbaca
Kevin cuadros <kevindavidcuadros@gmail.com>
2022-09-19 20:51:47 -0500

    scss config

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
   </div>
+  <h1>Hola Mundo</h1>
+  <button class="btn btn-primary" >Primary</button>
 </template>
 
 <script>
