6301a579b34f90bdf2a1db8fd5e12201ed51f761
Kevin cuadros <kevindavidcuadros@gmail.com>
2022-09-22 21:13:26 -0500

    base config vuex

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,23 @@
 export default () => ({
   isLoading: false,
-  entries: [],
+  entries: [
+    {
+      id: 1,
+      date: new Date().toDateString(),
+      text: 'Kevin ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+      picture: null,
+    },
+    {
+      id: 2,
+      date: new Date().toDateString(),
+      text: 'Juanas ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+      picture: null,
+    },
+    {
+      id: 3,
+      date: new Date().toDateString(),
+      text: 'Adoados ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+      picture: null,
+    },
+  ],
 })
